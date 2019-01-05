222bc9c880d000c96d8cbf9273e4a8b53457da6b
huangtao <taohuanga@163.com>
2019-01-05 22:24:43 +0800

    改版

--- curtain-order-web/src/main/java/com/towcent/curtain/order/web/mall/web/MallUserController.java
+++ curtain-order-web/src/main/java/com/towcent/curtain/order/web/mall/web/MallUserController.java
@@ -100,6 +100,7 @@ public class MallUserController extends BaseController {
             User sysUser = new User();
             sysUser.setId(null);
             sysUser.setLoginName(user.getLoginName());
+            sysUser.setJob("0");  // 待审核
             List<User> list = userService.findUser(sysUser);
             if (!CollectionUtils.isEmpty(list)) {
                 return resultVo(resultVo, E_001, "用户名已存在");
