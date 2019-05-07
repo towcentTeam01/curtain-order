57059ab46872679531f8a77ee38b423f679dc479
taohuanga <taohuanga@163.com>
2019-05-07 08:19:01 +0800

    改版

--- curtain-order-web/src/main/java/com/towcent/curtain/order/web/common/utils/ExportUtils.java
+++ curtain-order-web/src/main/java/com/towcent/curtain/order/web/common/utils/ExportUtils.java
@@ -108,7 +108,7 @@ public class ExportUtils {
         cell.setCellValue("联系电话：" + orderMain.getConsigneePhone());
         sheet.addMergedRegion(new CellRangeAddress(2, 2, 7, 10));
         
-        String[] headers = new String[]{"型号", "成品宽单位:米", "成品高单位:米", "褶数(倍)", "辅料铅线、铅块、底边花边", "里衬/造型(返幔、帘头)", "对/单开", "打孔/捏褶(对花)", "环、S钩(不要可不填)", "是否定型", "注明"};
+        String[] headers = new String[]{"型号", "成品宽单位:米", "成品高单位:米", "褶数(倍)", "辅料铅线、铅块、底边花边", "里衬/造型(返幔、帘头)", "对/单开", "打孔/捏褶(对花)", "环、S钩(不要可不填)", "工艺", "注明"};
         // 产生表格标题行
         row = sheet.createRow(3);
         style = getStyle2(wb);
@@ -137,7 +137,13 @@ public class ExportUtils {
             //createCell(wb, row, 7, "" + dtl.getNum());
             createCell(wb, row, 7, dtl.getParam4());
             createCell(wb, row, 8, dtl.getParam5());
-            createCell(wb, row, 9, "1".equals(dtl.getIsFinalize()) ? "是" : "否");
+            String gongyi = "";
+            if ("2".equals(dtl.getIsFinalize())) {
+                gongyi = "旗袍工艺";
+            } else if ("3".equals(dtl.getIsFinalize())) {
+                gongyi = "平针工艺";
+            }
+            createCell(wb, row, 9, gongyi);
             createCell(wb, row, 10, dtl.getRemarks());
             index++;
         } 
